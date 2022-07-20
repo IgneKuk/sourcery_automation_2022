9726b178b84838d514e744cab8d9c518b2b141b1
IgneKuk <igne.kukankaite@gmc.stud.vu.lt>
2022-07-20 10:17:03 +0300

    first commit

--- unit-testing/isTriangle.test.js
+++ unit-testing/isTriangle.test.js
@@ -2,8 +2,23 @@ const isTriangle = require('./isTriangle');
 
 test('3, 4, 5 is triangle', () => {
     expect(isTriangle(3, 4, 5)).toBe(true);
+    expect(isTriangle(3, 5, 4)).toBe(true);
+    expect(isTriangle(5, 4, 5)).toBe(true);
+
 });
 
-test('3, 4, 5 is triangle', () => {
-    expect(isTriangle(3, 4, 5)).toBe(false);
+test('-1, 4, 5 is not triangle', () => {
+    expect(isTriangle(-1, 4, 5)).toBe(false);
 });
+
+test('0, 4, 5 is not triangle', () => {
+    expect(isTriangle(0, 4, 5)).toBe(false);
+})
+
+test('A, 4, 5 is not triangle', () => {
+    expect(isTriangle('A', 4, 5)).toBe(false);
+})
+
+test('1.5, 4, 5 is triangle', () => {
+    expect(isTriangle(1.5, 4, 5)).toBe(true)
+})
